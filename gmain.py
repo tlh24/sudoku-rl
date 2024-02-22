fixed a scaling bug, but I think the idea of denoising the input to a transformer may be fundamentally flawed...

gmain.py
@@ -257,7 +257,7 @@ if __name__ == '__main__':
 	
 	# need to allocate hidden activations for denoising
 	record = []
-	yp,rp,a1,a2,w1,w2 = model.forward(x, a, msk, uu, record)
+	yp,rp,a1,a2,w1,w2 = model.forward(x, a, msk, uu, record) # dummy
 	denoisenet = []
 	denoiseopt = []
 	denoisestd = []
@@ -287,7 +287,7 @@ if __name__ == '__main__':
 			i = torch.arange(0,batch_size*stride) + u*batch_size*stride
 			hiddenl[j][i, :] = torch.reshape(h.detach(), (batch_size*stride, -1))
 		for h in hiddenl: 
-			std = torch.std(h)
+			std = torch.std(h) / 2.0
 			denoisestd.append(std)
 	
 	for j,net in enumerate(denoisenet): 
@@ -313,8 +313,9 @@ if __name__ == '__main__':
 					x = hidden[i,:]
 					t = torch.rand(batch_size).to(device)
 					tx = t.unsqueeze(-1).expand(-1,w)
-					z = torch.randn(batch_size, w).to(device) * tx * std
-					xz = torch.sqrt(1-tx)*x + torch.sqrt(tx)*z
+					z = torch.randn(batch_size, w).to(device) * std
+					# xz = torch.sqrt(1-tx)*x + torch.sqrt(tx)*z
+					xz = x + torch.sqrt(tx)*z
 				opt.zero_grad()
 				y = net.forward(xz,t)
 				loss = torch.sum((y - x)**2)

gracoonizer.py
@@ -67,13 +67,15 @@ class Gracoonizer(nn.Module):
 		reward = th.ones(batch_size) * 0.05
 		return y[:,:board_size,:], reward, a1, a2, w1, w2
 		
-	def backAction(self, benc, msk, n, newbenc, actual_action, lossmask, denoisenet, denoisestd): 
-		# pdb.set_trace()
+	def backAction(self, benc, msk, n, newbenc, actual_action, lossmask, denoisenet, denoisestd):
+		# record the real targets for the internal variables. 
+		record_true = []
+		self.forward(benc, actual_action, msk, n, record_true)
+		
 		batch_size = benc.shape[0]
 		actnodes = graph_encoding.sudokuActionNodes(-1) # null move.
 		_,actenc,_ = graph_encoding.encode_nodes([], actnodes) 
-		actenc = np.tile(actenc, [batch_size, 1, 1])
-		# actenc = actual_action.cpu().detach().numpy()
+		actenc = np.tile(actenc, [batch_size, 1, 1]) # tile the null move
 		action = th.tensor(actenc, requires_grad=True, device=benc.device)
 		N = 500
 		loss = np.zeros((N,5))
@@ -81,13 +83,14 @@ class Gracoonizer(nn.Module):
 			temp = (N - i) / (N+2.0)
 			self.zero_grad()
 			record = []
+			# pdb.set_trace()
 			y,_,_,_,_,_ = self.forward(benc, action, msk, n, record)
 			y = y * lossmask
 			err = th.sum((y - newbenc)**2)
 			err.backward(retain_graph=True)
 			loss[i,0] = err.cpu().detach().item()
 			x = th.cat((benc, action), axis=1)
-			losses = self.xfrmr.backAction(x, msk, newbenc, record, denoisenet, denoisestd, temp)
+			losses = self.xfrmr.backAction(x, msk, newbenc, record, denoisenet, denoisestd, temp, record_true)
 			for j,l in enumerate(losses):
 				loss[i,j+1] = l
 			print(loss[i])
@@ -126,6 +129,7 @@ class Gracoonizer(nn.Module):
 				axs[1,2].plot(loss[:,3], "b",label="h2")
 				axs[1,2].plot(loss[:,4], "m",label="h3")
 				axs[1,2].set_title('loss over new board encoding')
+				axs[1,2].legend()
 				plt.show()
 		return action
 		

graph_transformer.py
@@ -244,8 +244,12 @@ class Transformer(nn.Module):
 		self.layer1.allHeadsOn()
 		self.layer2.allHeadsOn()
 
-	def backAction(self, x, msk, y, record, denoisenet, denoisestd, temp): 
+	def backAction(self, x, msk, y, record, denoisenet, denoisestd, temp, record_true): 
 		# x needs to have grad on! 
+		doplot = False
+		if doplot: 
+			fig, axs = plt.subplots(4, 20, figsize=(30,20))
+		
 		losses = []
 		# accumulate gradients from the denoised hidden states. 
 		for j,h in enumerate(record):
@@ -253,10 +257,22 @@ class Transformer(nn.Module):
 			with torch.no_grad(): 
 				net = denoisenet[j]
 				std = denoisestd[j]
-				z = torch.randn_like(h) * std * math.sqrt(temp)
+				z = torch.randn_like(h) * std * math.sqrt(temp) * 0.1
 				t = torch.ones(h.shape[0], device=x.device) * temp
 				hdn = net.forward(h+z,t)
 			loss = torch.sum(0.001*(h - hdn)**2) # ??
 			loss.backward(retain_graph=True)
 			losses.append(loss.detach().cpu().item())
+			
+			if doplot: 
+				if j < 4: 
+					ht = record_true[j]
+					ht = torch.reshape(ht, (ht.shape[0]*ht.shape[1], ht.shape[2]))
+					for r in range(20): 
+						axs[j, r].plot(ht[r,:].detach().cpu().numpy().T, 'k')
+						axs[j, r].plot(h[r,:].detach().cpu().numpy().T, 'b')
+						axs[j, r].plot(hdn[r,:].detach().cpu().numpy().T, 'r')
+						axs[j, r].set_title(f'{j} k=truth; b=estimate; r=denoised')
+			
+		plt.show()
 		return losses

mnist.py
@@ -395,7 +395,7 @@ def main():
 			indata[i*batch_size:(i+1)*batch_size, :, :] = torch.squeeze(data)
 		print('done generating hidden')
 		
-		N = 4000
+		N = 40000
 		losses = np.zeros((N,))
 		
 		print('training hidden denoiser')
@@ -405,7 +405,7 @@ def main():
 				t = torch.rand(batch_size).to(device)
 				tx = t.unsqueeze(-1).expand((-1, H))
 				x = hidden[i,:]
-				z = torch.randn_like(x) * tx * 2.5
+				z = torch.randn_like(x) * 2.5 
 				xz = torch.sqrt(1-tx)*x + torch.sqrt(tx)*z
 			hdenoiseopt.zero_grad()
 			y = hdenoise.forward(xz,t)
@@ -421,7 +421,7 @@ def main():
 			# 	plt.legend()
 			# 	plt.show()
 			
-		N = 6000
+		N = 60000
 		losses = np.zeros((N,))
 		print('training input denoiser')
 		for u in range(N): 
@@ -430,7 +430,7 @@ def main():
 				t = torch.rand(batch_size).to(device) # temperature
 				tx = t.unsqueeze(-1).expand((-1, 784))
 				x = torch.reshape(indata[i,:,:], (batch_size, 784))
-				z = torch.randn(batch_size, 784).to(device) * tx * 0.5
+				z = torch.randn(batch_size, 784).to(device) * 0.5 
 				xz = torch.sqrt(1-tx)*x + torch.sqrt(tx)*z
 			idenoiseopt.zero_grad()
 			y = idenoise.forward(xz,t)