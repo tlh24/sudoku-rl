c8b9f47f4a29c284d90eeca9d94a1739b3344049
Justin Jung <justin@springtail.ai>
2024-03-05 19:05:19 -0500

    Comment sudoku and sudoku puzzle generation files

--- sudoku_gen_many.py
+++ sudoku_gen_many.py
@@ -1,22 +1,27 @@
+'''
+Generates N sudoku maps with some positions blank and some filled. 
+Saves the tensor of maps as a torch file named puzzles_{N}.pt
+'''
 import math
 import torch
 import numpy as np
+from constants import sudoku_width
 from sudoku_gen import Sudoku
 import matplotlib.pyplot as plt
 import model
 from multiprocessing import Pool
 from itertools import product
 
+
 # the naive sudoku generator is slow. 
 # parallelize and save the results for fast loading later.
 
 N = 500000
-
-x = torch.zeros(N, 9, 9)
+x = torch.zeros(N, sudoku_width, sudoku_width)
 
 def makePuzzle(j): # argument is ignored.
 	k = np.random.randint(20) + 25 # how many positions to blank.
-	sudoku = Sudoku(9, k)
+	sudoku = Sudoku(sudoku_width, k)
 	sudoku.fillValues()
 	return torch.tensor(sudoku.mat)
 
