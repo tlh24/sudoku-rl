fixed a dumb bug in the mask calculation.

gmain.py
@@ -242,7 +242,7 @@ if __name__ == '__main__':
 	use_adamw = False
 	
 	if use_adamw: 
-		optimizer = optim.AdamW(model.parameters(), lr=1e-3, weight_decay = 5e-2)
+		optimizer = optim.AdamW(model.parameters(), lr=1e-2, weight_decay = 5e-2)
 		
 	else: 
 		optimizer = psgd.LRA(model.parameters(),lr_params=0.01,lr_preconditioner=0.01, momentum=0.9,preconditioner_update_probability=0.1, exact_hessian_vector_product=False, rank_of_approximation=10, grad_clip_max_norm=5)
@@ -262,21 +262,25 @@ if __name__ == '__main__':
 			y = new_board_enc[i,:,:].to(device) 
 			reward = board_reward[i]
 			
-			# if use_adamw: 
-			# optimizer.zero_grad()
-			yp,rp,a1,a2,w1,w2 = model.forward(x, a, msk, u, None)
-			# yp = yp * lossmask
-			# loss = torch.sum((yp - y)**2)
-			# loss.backward()
-			# optimizer.step() 
-			# else: 
+			
 			def closure():
 				yp,rp,a1,a2,w1,w2 = model.forward(x, a, msk, u, None)
 				yp = yp * lossmask
 				loss = torch.sum((yp - y)**2) + sum( \
                 [torch.sum(1e-4 * torch.rand_like(param) * param * param) for param in model.parameters()])
 				return loss
-			loss = optimizer.step(closure)
+			
+			if use_adamw: 
+				optimizer.zero_grad()
+				yp,rp,a1,a2,w1,w2 = model.forward(x, a, msk, u, None)
+				yp = yp * lossmask
+				loss = torch.sum((yp - y)**2)
+				loss.backward()
+				optimizer.step() 
+			else:
+				loss = optimizer.step(closure)
+				if u % 25 == 0: 
+					yp,rp,a1,a2,w1,w2 = model.forward(x, a, msk, u, None)
             
 			print(loss.cpu().item())
 			fd_losslog.write(f'{uu}\t{loss.cpu().item()}\n')

graph_encoding.py
@@ -134,11 +134,13 @@ def sudokuToNodes(puzzle, guess_mat, curs_pos, action_type:int, action_val:int):
 				nb.add_child(nby)
 				nb.add_child(nbb)
 				
+				highlight = 0
 				if x == curs_pos[0] and y == curs_pos[1]: 
-					nbh = Node(Types.POSITION, Axes.H_AX)
-					nbhh = Node(Types.LEAF, 1) # redundant, but ok
-					nbh.add_child(nbhh)
-					nb.add_child(nbh)
+					highlight = 1
+				nbh = Node(Types.POSITION, Axes.H_AX)
+				nbhh = Node(Types.LEAF, highlight) # redundant, but ok
+				nbh.add_child(nbhh)
+				nb.add_child(nbh)
 				
 				if guess_mat[y,x] != 0: 
 					nbg = Node(Types.GUESS, guess_mat[y,x])
@@ -165,28 +167,33 @@ def encodeNodes(bnodes, actnodes):
 	benc = np.zeros((bcnt, 20), dtype=np.float32)
 	actenc = np.zeros((actcnt, 20), dtype=np.float32)
 	
-	def encodeNode(i, node, encoding): 
+	def encodeNode(i, m, node, encoding): 
 		'''
 		Recursive function which populates the encoding matrix.
 		Each encoded vector of the node (a row) contains a one-hot encoding of the node type
 			(i.e cursor, position, leaf, box, action) and also contains the node value
 		The recursion is such that the order is DFS 
+		note: 
+			i is reset between passes 
+			m is not (gobal index to the mask)
 		'''
 		encoding[i, node.typ.value] = 1.0 # categorical
 		# enc[i, node.value + 10] = 1.0 # categorical
 		encoding[i, 10] = node.value # ordinal 
-		node.loc = i # save loc for mask.
+		node.loc = m # save loc for mask.
 		i = i + 1
+		m = m + 1
 		for k in node.kids: 
-			i = encodeNode(i, k, encoding)
-		return i
+			i,m = encodeNode(i, m, k, encoding)
+		return i,m
 			
 	i = 0
+	m = 0
 	for n in bnodes: 
-		i = encodeNode(i, n, benc)
+		i,m = encodeNode(i, m, n, benc)
 	i = 0
 	for n in actnodes: 
-		i = encodeNode(i, n, actenc)
+		i,m = encodeNode(i, m, n, actenc)
 	
 	nodes = bnodes + actnodes
 	cnt = bcnt + actcnt

plot_mmap.py
@@ -108,7 +108,7 @@ if __name__ == "__main__":
 	while True:
 		# i = np.random.randint(batch_size) # checking
 		i = 0
-		cl = 40
+		cl = token_cnt
 			
 		if mode == 0: 
 			board = read_mmap(fd_board, [batch_size, token_cnt, world_dim])