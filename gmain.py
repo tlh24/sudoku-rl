6a344b45f3943dc9246c7a8b78b57fcc3bb6b549
Tim Hanson <sideskate@gmail.com>
2024-07-16 12:05:24 -0700

    start with clean board for the movement predictor..

--- gmain.py
+++ gmain.py
@@ -493,7 +493,7 @@ def trainQfun(rollouts_board, rollouts_reward, rollouts_action, nn, memory_dict,
 					model_boards,_,_ = model.forward(boards,hcoo,0,None)
 				qfun_boards,_,_ = qfun.forward(model_boards,hcoo,0,None)
 			reward_preds = qfun_boards[:,reward_loc, 32+26]
-			pred_data = {'old_board':boards, 'new_board':model_boards, 'new_state_preds':qfun_boards,
+			pred_data = {'old_board':boards, 'new_board':qfun_boards, 'new_state_preds':qfun_boards,
 								'rewards': reward, 'reward_preds': reward_preds,
 								'w1':None, 'w2':None}
 			loss = torch.sum((reward - reward_preds)**2) + \
