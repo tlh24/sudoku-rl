ed7bc62c5e035fe93dd156aa539991e0fc6372af
Tim Hanson <sideskate@gmail.com>
2024-07-20 15:08:13 -0700

    common changes

--- mnist_zeroinit.sh
+++ mnist_zeroinit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-train_size=1000
+train_size=100
 
 python mnist_zeroinit.py --train-size $train_size -a -z -c 0 &
 python mnist_zeroinit.py --train-size $train_size -a -z -c 0 &
@@ -13,3 +13,5 @@ python mnist_zeroinit.py --train-size $train_size -a -c 0 &
 python mnist_zeroinit.py --train-size $train_size -a -c 0 &
 python mnist_zeroinit.py --train-size $train_size -a -c 1 &
 python mnist_zeroinit.py --train-size $train_size -a -c 1 &
+
+wait
