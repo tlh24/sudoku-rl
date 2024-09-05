8ba081591ca54b5afc17c6c8753a0f7217efdd5e
Justin Jung <justinsoljung@gmail.com>
2024-09-05 14:53:12 -0700

    run sedd on sudoku puzzle boards

--- data/sudoku_trajs/test.py
+++ data/sudoku_trajs/test.py
@@ -9,7 +9,13 @@ from utils import check_if_valid_state_traj, check_if_solved, action_traj_idxs_u
 
 
 if __name__ == "__main__":
-    file = 'forward/action/10000_trajs_0.45_filled_yes_head.npy'
+    file = 'forward/state/1000_trajs_0.45_filled_no_head.npy'
+    trajs = np.load(file)
+    for i in range(len(trajs)):
+        print(trajs[i].shape)
+    breakpoint()
+
+    #file = 'forward/action/10000_trajs_0.45_filled_yes_head.npy'
     action_trajs = np.load(file)
     for i in range(len(action_trajs)):
         is_unique = action_traj_idxs_unique(action_trajs[i])
