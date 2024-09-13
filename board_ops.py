8d4a92cfb96621ab5a0312a886a018b001fa94f9
Tim Hanson <sideskate@gmail.com>
2024-09-13 11:12:14 -0700

    made the initial board generation parallel.

--- board_ops.py
+++ board_ops.py
@@ -158,7 +158,7 @@ def enumerateBoards(puzzles):
 	n_actions = len(action_types) # 13
 	n_curspos = 3
 	n_masks = 3
-	n_puzzles = 512 # 1024, 1280 1536 2048
+	n_puzzles = 2048 # 1024, 1280 1536 2048
 	n = n_actions * n_masks * n_curspos * n_puzzles
 	
 	try:
