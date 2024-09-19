f51a1122c8094467b1a0bc150b6d763df93fff31
Tim Hanson <sideskate@gmail.com>
2024-09-19 14:33:25 -0700

    much simpler network should work!

--- pmain.py
+++ pmain.py
@@ -111,7 +111,7 @@ if __name__ == "__main__":
 	args['fd_losslog'] = fd_losslog
 
 	model = Gracoonizer(xfrmr_dim=xfrmr_dim, world_dim=world_dim, \
-		n_heads=n_heads, n_layers=5, repeat=5, mode=0).to(device)
+		n_heads=n_heads, n_layers=2, repeat=8, mode=0).to(device)
 	model.printParamCount()
 
 	try:
@@ -127,7 +127,8 @@ if __name__ == "__main__":
 	optimizer = gmain.getOptimizer(optimizer_name, model)
 
 	hcoo = gmain.expandCoordinateVector(coo, a2a)
-	hcoo.append('dense') # for dense attention.
+	# hcoo.append('dense') # for dense attention.
+	hcoo = hcoo[0:2] # only the sparse / set-layers.
 
 	input_thread = threading.Thread(target=utils.monitorInput, daemon=True)
 	input_thread.start()
