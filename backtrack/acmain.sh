abb1d29407931d2facf53b12c7391bb45c651602
Tim Hanson <sideskate@gmail.com>
2024-11-14 21:56:48 -0800

    trying overnight run

--- backtrack/acmain.sh
+++ backtrack/acmain.sh
@@ -2,6 +2,7 @@
 for i in {1..20}
 do
 ./backup.sh
-parallel -j4 -u --link 'python acmain.py -b 64 --puzz 3 -r 2 -i {1} --cuda {2}' ::: 3 2 1 0 ::: 3 2 1 0
+# parallel -j4 -u --link 'python acmain.py -b 64 --puzz 3 -r 2 -i {1} --cuda {2}' ::: 3 2 1 0 ::: 3 2 1 0
+parallel -j8 -u --link 'python acmain.py -b 64 --puzz 1 -r 2 -i {1} --cuda {2}' ::: 0 1 2 3 4 5 6 7 ::: 3 2 1 0 3 2 1 0
 python acmain.py -r 2
 done
