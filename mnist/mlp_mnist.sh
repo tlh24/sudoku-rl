1448d645dacbd0974da8ae99d03709ff612024d7
Tim Hanson <sideskate@gmail.com>
2025-01-10 13:40:17 -0800

    need to do another run.

--- mnist/mlp_mnist.sh
+++ mnist/mlp_mnist.sh
@@ -4,6 +4,8 @@ python mlp_mnist.py --hidden 256
 python mlp_mnist.py --hidden 512
 python mlp_mnist.py --hidden 1024
 python mlp_mnist.py --hidden 2048
+python mpl_mnist.py --hidden 3072
 python mlp_mnist.py --hidden 4096
-# python mlp_mnist.py --hidden 8192
+python mpl_mnist.py --hidden 6144
+python mlp_mnist.py --hidden 8192
 
